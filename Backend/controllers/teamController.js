a302c9a15cd2e6b09544b6747dccc04ba879efc4
Endale Teggnework <endalework90@gmail.com>
2022-06-04 22:12:11 +0300

    populating clubId on my team

--- Backend/controllers/teamController.js
+++ Backend/controllers/teamController.js
@@ -18,10 +18,10 @@ exports.createNewTeam = catchAsync(async (req, res, next) => {
 exports.getAllTeams = catchAsync(async (req, res, next) => {
   console.log(req.query);
   const teams = await VirtualTeam.find(req.query).populate([
-    { path: 'team.keepers', populate: ['player'] },
-    { path: 'team.defenders', populate: ['player'] },
-    { path: 'team.midfielders', populate: ['player'] },
-    { path: 'team.forwards', populate: ['player'] },
+    { path: 'team.keepers.player', populate: ['clubId'] },
+    { path: 'team.defenders.player', populate: ['clubId'] },
+    { path: 'team.midfielders.player', populate: ['clubId'] },
+    { path: 'team.forwards.player', populate: ['clubId'] },
   ]);
   // SEND RESPONSE
   res.status(200).json({
