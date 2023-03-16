92bfeb6651aa8ec22db7a939347e9b081129e455
Endale Teggnework <endalework90@gmail.com>
2023-03-16 15:18:35 +0300

    modification in main files and remove providers and widgets folder

--- frontend/Client/lib/main.dart
+++ frontend/Client/lib/main.dart
@@ -1,32 +1,139 @@
-// @dart=2.9
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'flutter_flow/flutter_flow_theme.dart';
+import 'flutter_flow/flutter_flow_util.dart';
+import 'flutter_flow/internationalization.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'index.dart';
 
-// import './screens/auth_screen.dart';
-import './widgets/login.dart';
-import './providers/auth.dart';
-// import './widgets/create_team.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() => runApp(Fantasy());
+  await FlutterFlowTheme.initialize();
 
-class Fantasy extends StatelessWidget {
-  // const Fantasy({Key key}) : super(key: key);
+  FFAppState(); // Initialize FFAppState
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>();
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale _locale;
+  ThemeMode _themeMode = FlutterFlowTheme.themeMode;
+
+  void setLocale(Locale value) => setState(() => _locale = value);
+  void setThemeMode(ThemeMode mode) => setState(() {
+        _themeMode = mode;
+        FlutterFlowTheme.saveThemeMode(mode);
+      });
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(value: Auth()),
-        // ChangeNotifierProvider.value(value:Players()),
+    return MaterialApp(
+      title: 'eplff',
+      localizationsDelegates: [
+        FFLocalizationsDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
       ],
-      child: MaterialApp(
-        title: 'Fantasy Football',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-          // accentColor: Colors.deepOrange,
-          fontFamily: 'Lato',
-        ),
-        home: LoginPage(),
+      locale: _locale,
+      supportedLocales: const [Locale('en', '')],
+      theme: ThemeData(brightness: Brightness.light),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: _themeMode,
+      home: NavBarPage(),
+    );
+  }
+}
+
+class NavBarPage extends StatefulWidget {
+  NavBarPage({Key key, this.initialPage}) : super(key: key);
+
+  final String initialPage;
+
+  @override
+  _NavBarPageState createState() => _NavBarPageState();
+}
+
+/// This is the private State class that goes with NavBarPage.
+class _NavBarPageState extends State<NavBarPage> {
+  String _currentPage = 'HomePage';
+
+  @override
+  void initState() {
+    super.initState();
+    _currentPage = widget.initialPage ?? _currentPage;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final tabs = {
+      'HomePage': HomePageWidget(),
+      'LoginandSignup': LoginandSignupWidget(),
+      'LiveScore': LiveScoreWidget(),
+      'Highlights': HighlightsWidget(),
+    };
+    final currentIndex = tabs.keys.toList().indexOf(_currentPage);
+    return Scaffold(
+      body: tabs[_currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (i) => setState(() => _currentPage = tabs.keys.toList()[i]),
+        backgroundColor: Colors.black,
+        selectedItemColor: Color(0xFFDFE3E4),
+        unselectedItemColor: FlutterFlowTheme.of(context).primaryBtnText,
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+              size: 28,
+            ),
+            activeIcon: Icon(
+              Icons.home,
+              size: 24,
+            ),
+            label: 'Latest',
+            tooltip: '',
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.fantasyFlightGames,
+              size: 29,
+            ),
+            label: 'Fantasy',
+            tooltip: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.sports_soccer,
+              size: 28,
+            ),
+            label: 'fixtures',
+            tooltip: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.live_tv_outlined,
+              size: 24,
+            ),
+            label: 'Highlights',
+            tooltip: '',
+          )
+        ],
       ),
     );
   }
