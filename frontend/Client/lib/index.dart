4578d94059ac94c8f69c0648473f35a97259436b
Endale Teggnework <endalework90@gmail.com>
2023-03-16 15:23:28 +0300

    application state files and index files are added

---
+++ frontend/Client/lib/index.dart
@@ -0,0 +1,24 @@
+// Export pages
+export 'home_page/home_page_widget.dart' show HomePageWidget;
+export 'transfers/transfers_widget.dart' show TransfersWidget;
+export 'loginand_signup/loginand_signup_widget.dart' show LoginandSignupWidget;
+export 'fantasy_homepage/fantasy_homepage_widget.dart'
+    show FantasyHomepageWidget;
+export 'pick_team/pick_team_widget.dart' show PickTeamWidget;
+export 'points/points_widget.dart' show PointsWidget;
+export 'league/league_widget.dart' show LeagueWidget;
+export 'join_leagues/join_leagues_widget.dart' show JoinLeaguesWidget;
+export 'noobie_transfer/noobie_transfer_widget.dart' show NoobieTransferWidget;
+export 'customize_team/customize_team_widget.dart' show CustomizeTeamWidget;
+export 'defrnders_list/defrnders_list_widget.dart' show DefrndersListWidget;
+export 'live_score/live_score_widget.dart' show LiveScoreWidget;
+export 'highlights/highlights_widget.dart' show HighlightsWidget;
+export 'keepers_list/keepers_list_widget.dart' show KeepersListWidget;
+export 'mid_fielders_list/mid_fielders_list_widget.dart'
+    show MidFieldersListWidget;
+export 'forwards/forwards_widget.dart' show ForwardsWidget;
+export 'def_list_pro/def_list_pro_widget.dart' show DefListProWidget;
+export 'g_klistpro/g_klistpro_widget.dart' show GKlistproWidget;
+export 'mf_list/mf_list_widget.dart' show MfListWidget;
+export 'mf_listpro/mf_listpro_widget.dart' show MfListproWidget;
+export 'fwd_list_pro/fwd_list_pro_widget.dart' show FwdListProWidget;
